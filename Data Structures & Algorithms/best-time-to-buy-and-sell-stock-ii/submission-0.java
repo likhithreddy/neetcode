3e560e5a83f2b8af198b52c3064eb0e1a889fbc4
Likhith Reddy Rechintala <54302844+likhithreddy@users.noreply.github.com>
2026-06-13 10:16:35 -0400

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            int profit = prices[i + 1] - prices[i];
+            if (profit > 0) {
+                maxProfit += profit;
+            }
+        }
+        return maxProfit;
+    }
+}
